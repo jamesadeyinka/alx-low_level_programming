84bc1c098c13d0b68bff796c56b6d4a62e5990b0
jamesadeyinka <princessadeyinkaaa@gmail.com>
2023-02-19 12:46:28 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,15 +10,14 @@ int main(void)
 {
 
 	char ch;
-	char n;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
 	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
-	
-	putchar(n);
+
+	putchar('\n');
 
 	return (0);
 }
