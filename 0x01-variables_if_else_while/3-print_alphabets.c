6d433a2ca91296222f85aa41e41d004b50433b90
jamesadeyinka <princessadeyinkaaa@gmail.com>
2023-02-19 08:24:07 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,17 +3,20 @@
 /**
  * main - prints alpha in lower and upper case
  * Return: 0
- */
+*/
+
 int main(void)
+
 {
+
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar (ch);
+		putchar(ch);
 
 	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
-
+	
 	putchar(n);
 
 	return (0);
