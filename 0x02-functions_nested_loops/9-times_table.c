8439fc8a98feb7f47494e69362ce84dccd5366d4
jamesadeyinka <princessadeyinkaaa@gmail.com>
2023-02-24 11:26:52 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *time_time - print the 9 timees table
+ */
+void times_table(void)
+{
+	int num, mult prod;
+	for (num =0; num <= 9; num++)
+	{
+
+		_putchar('0');
+
+	
+		for (mult = 1; mult <= 9: mult+=)
+	{
+		_putchar(',');
+		_putchar(' ');
